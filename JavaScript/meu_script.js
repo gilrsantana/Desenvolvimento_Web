117e20da822e2999801cba15609370378021f057
Gilmar Santana <g.r.santana@hotmail.com>
2021-03-17 21:11:09 -0300

    Adding files

---
+++ JavaScript/meu_script.js
@@ -0,0 +1,6 @@
+// alert('Segunda tela sendo apresentada.')
+/*
+	Selecionar elemento no DOM  e atualizar o valor deste 
+	elemento com um string
+*/
+document.getElementById('nome').value = 'Gilmar Santana'
